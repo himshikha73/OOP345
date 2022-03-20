aded52ff0d539e6126a99de9ea2e07e4799976d5
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2022-03-20 13:57:51 -0400

    first commit

---
+++ w7p1/CovidCollection.cpp
@@ -0,0 +1,12 @@
+//==============================================
+// Name:           Volodymyr Labliuk
+// Student Number: 147302202
+// Email:          vlabliuk@myseneca.ca
+// Section:        NBB
+// Date:           17.03.2022
+//==============================================
+#define  _CRT_SECURE_NO_WARNINGS
+using namespace std;
+namespace sdds
+{
+};
---
+++ w7p1/CovidCollection.h
@@ -0,0 +1,19 @@
+//==============================================
+// Name:           Volodymyr Labliuk
+// Student Number: 147302202
+// Email:          vlabliuk@myseneca.ca
+// Section:        NBB
+// Date:           17.03.2022
+//==============================================
+
+#ifndef SDDS_FILE_H
+#define SDDS_FILE_H
+#include <string>
+namespace sdds
+{
+	class CovidCollection
+	{
+
+	};
+}
+#endif
