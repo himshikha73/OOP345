956ef15aee0beb24f50299b1ddec789067f0740b
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2022-03-23 23:02:30 -0400

    first commit

---
+++ w7p2/CovidCollection.h
@@ -0,0 +1,37 @@
+//==============================================
+// Name:           Volodymyr Labliuk
+// Student Number: 147302202
+// Email:          vlabliuk@myseneca.ca
+// Section:        NBB
+// Date:           23.03.2022
+//==============================================
+
+#ifndef SDDS_FILE_H
+#define SDDS_FILE_H
+#include <string>
+#include <vector>
+namespace sdds
+{
+
+	struct Covid
+	{
+		std::string m_country{};
+		std::string m_city{};
+		std::string m_variant{};
+		size_t m_cases{};
+		int m_year{};
+		size_t m_deaths{};
+	};
+
+	class CovidCollection
+	{
+	private:
+		std::vector<Covid> m_infections;
+	public:
+		CovidCollection(std::string fileName);
+		void display(std::ostream& out) const;
+
+	};
+	std::ostream& operator<<(std::ostream& out, const Covid& theCovid);
+}
+#endif
