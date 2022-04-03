b0f25c4b8350d9e32af0fe2e6dbd41dd3b75f8ea
Vladimir Labliuk <50242030+n1ceFella@users.noreply.github.com>
2022-04-03 19:54:19 -0400

    raw pointer changed to smart pointers

---
+++ w8p2/WritingRecord.h
@@ -0,0 +1,25 @@
+//==============================================
+// Name:           Volodymyr Labliuk
+// Student Number: 147302202
+// Email:          vlabliuk@myseneca.ca
+// Section:        NBB
+// Date:           03.04.2022
+//==============================================
+
+#ifndef SDDS_WRITINGRECORD_H
+#define SDDS_WRITINGRECORD_H
+
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include "GeneratingList.h"
+#include "EmpProfile.h"
+
+namespace sdds {
+	GeneratingList<EmployeeWage> writeRaw(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal);
+	GeneratingList<EmployeeWage> writeSmart(const GeneratingList<Employee>& emp, const GeneratingList<Salary>& sal);
+}
+
+
+#endif // SDDS_WRITINGRECORD_H
+
